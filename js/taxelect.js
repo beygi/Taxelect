554b8c9ecb46a2e121e6a23a5118fa0392157173
Taxellool <taxellool@gmail.com>
2014-07-21 13:00:30 +0430

    #2

---
+++ js/taxelect.js
@@ -0,0 +1,151 @@
+(function ($){
+	$.fn.taxelect = function (options) {
+		// set the default values
+		defaults = {
+			hasIcons: false,
+			width: 190,
+			color: '#909597',
+			defaultOption: '-1',
+			defaultString: 'لطفا انتخاب کنید:‌',
+			optionHeight: 30,
+			closedHeight: 30,
+			openedHeight: 120,
+			openingSpeed: 500,
+			closingSpeed: 500,
+			closeOtherSpeed: 200
+		},
+		settings = $.extend({},defaults, options);
+		
+		this.each(function(){
+			var selectOptions = [],
+				parent        = $(this).parent(),
+				selectName    = $(this).attr('name'),
+				selectFormId  = $(this).parents('form').attr('id'),
+				hasSearch     = $(this).data('taxelect-search') ? true: false,
+				target        = '';
+			// make a list of needed options
+			$(this).children().each(function(){
+				selectOptions.push($(this).text());
+			});
+			// build the html structure
+			var tmp = '';
+			for (var i = 0; i < selectOptions.length; i++) {
+				tmp += '<li class="taxelect-option" data-option="' 
+				+ i + '">' + selectOptions[i] + '</li>\n';
+			};
+			if (hasSearch){
+				tmp = '<li class="taxearch">\n<input type="text">' + tmp + '</li>\n'
+			}
+			// add needed elements after select
+			$(this).after('<div class="taxelect-wrapper">\n' 
+				+ '<ul class="taxelect-ul" data-form-id="' 
+				+ selectFormId + '" data-name="' 
+				+ selectName + '">\n'
+				+ '<li class="taxelected" data-default-option="' 
+				+ settings.defaultOption 
+				+ '">' + settings.defaultString + '</li>' 
+				+ tmp + '\n</ul>\n</div>');
+			// assign added elements to variables
+			var selectWrapper = $(this).siblings('.taxelect-wrapper'),
+				dropdownItems = selectWrapper.find('.taxelect-option'),
+				taxearchInput = selectWrapper.find('input');
+			// apply settings on result element
+			selectWrapper.css({
+				width: settings.width,
+				color: settings.color
+			});
+			// apply settings on dropdown options 
+			dropdownItems.css({
+				height: settings.optionHeight,
+				lineHeight: settings.optionHeight + 'px'
+			})
+
+			// open a dropdown on menu on click
+			selectWrapper.click(function(){
+				var other = $('#' + selectFormId + ' .opened');
+				if (selectWrapper.hasClass('opened')) {
+					selectWrapper.animate({height: settings.closedHeight}, settings.closingSpeed, function(){
+						selectWrapper.removeClass('opened');
+					});
+					selectWrapper.mCustomScrollbar('disable',true);
+				}
+				else{
+					selectWrapper.mCustomScrollbar('update');
+					if (other.length){
+						selectWrapper.addClass('opened').promise().done(function(){
+							selectWrapper.animate({height: settings.openedHeight}, settings.openingSpeed);
+						});
+						other.animate({height: settings.closedHeight},settings.closeOtherSpeed ,function(){
+							other.removeClass('opened');
+						});
+					}else{
+						selectWrapper.addClass('opened').promise().done(function(){
+							selectWrapper.animate({height: settings.openedHeight}, settings.openingSpeed);
+							if ((selectWrapper.find('li').length * settings.optionHeight) > (settings.openedHeight - settings.closedHeight)){
+								selectWrapper.mCustomScrollbar({
+							    	theme: "minimal-dark"
+							    });
+							}
+						});
+					}
+				}
+			}).find('.taxearch').click(function(e){
+				return false;
+			});
+			// sets the selected option as selected and closes the dropdown
+			dropdownItems.click(function(){
+				var elem      = $(this),
+					parent    = elem.parent(),
+					formId    = parent.data('form-id'),
+					inputName = parent.data('name'),
+					wrapper   = elem.parents('.taxelect-wrapper');
+				elem.siblings('.taxelected').html($(this).html());
+				$('#' + formId + ' select[name="' + inputName + '"]').prop('selectedIndex', elem.data('option'));
+			}).hover(function(){
+				if(!$(this).hasClass('chosen')){
+					$(this).siblings().removeClass('chosen');
+					$(this).addClass('chosen');
+				}
+			}, function(){
+				$(this).removeClass('chosen');
+			});
+			//set the setting for search input
+			taxearchInput.css({
+				width: settings.width - 25
+			});
+			// hide original select
+			return $(this).css({
+				display: 'none'
+			});
+			$(this).prop('selectedIndex', function(){
+				return $('.taxelect-ul[data-form-id="' + selectFormId + '"][data-name="' + selectName + '"] > .taxelected').data('default-option');
+			});
+		});
+		// searches through select options
+		$('.taxearch > input').bind("change paste keyup", function(){
+			var value   = $(this).val(),
+				parent  = $(this).parents('.taxelect-wrapper'),
+				options = $(this).parent().siblings('.taxelect-option');
+			options.each(function(){
+				if (!$(this).html().indexOf(value)){
+					$(this).css({display: ''});
+				}else{
+					$(this).css({display: 'none'});
+				}
+			});
+			if (parent.find('.option:not(.hidden)').length > 4){
+				parent.mCustomScrollbar("update");
+			}
+		});
+		// closes all the opened dropdowns upon clicking anywhere outside them.
+		$(document).on('click', function(event) { 
+			if (!$(event.target).closest('.taxelect-wrapper').length) {
+				$('.taxelect-wrapper.opened').each(function(){
+					$(this).animate({height: settings.closedHeight}, function(){
+						$(this).removeClass('opened');
+					});
+				});
+			}
+		});
+	};
+}(jQuery));
